f8d0b168446fbf9d3ebc5d81716db8554720db55
SanjidaMitu123 <sanjidamitu456@gmail.com>
2023-08-31 22:55:05 +0600

    card added

--- tube.js
+++ tube.js
@@ -8,7 +8,7 @@ const categorylist = async () => {
        
         const div = document.createElement("div");
         div.innerHTML = `
-        <a class="tab">${cat.category}</a> 
+        <a onclick="loadvideos('${cat.category_id}')" class="tab">${cat.category}</a> 
         `;
 
     catContainer.appendChild(div);
@@ -16,5 +16,34 @@ const categorylist = async () => {
     });
     console.log(data.data);
 };
+
+const loadvideos = async (catId) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${catId}`);
+    const data = await res.json();
+    console.log(data.data);
+
+    const cards = document.getElementById("card-container");
+
+    data.data.forEach((videos)=>{
+        console.log(videos);
+        const div = document.createElement('div');
+        div.innerHTML=` <div class="card w-66 h-[400px] bg-base-100 m-6 shadow-xl">
+        <figure><img src=${videos.thumbnail} /></figure>
+        <div class="card-body">
+          <h2 class="card-title"> <img src=${videos?.authors?.[0].profile_picture} class="w-10 h-10 rounded-full" /> ${videos.title}</h2>
+          <p>${videos?.authors?.[0].profile_name} 
+          
+          </p>
+          <p>${videos.others.views}</p>
+          <div class="">
+          </div>
+        </div>
+      </div>`;
+      cards.appendChild(div);
+
+    });
+
+};
+
 
 categorylist();
